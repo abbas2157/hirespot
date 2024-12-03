93bd8d6da0d676d39011f3a96c844b6fd9c44f0b
ismailwatto <mismailwatto195@gmail.com>
2024-12-03 11:09:49 +0500

    skill set for user with suggestion

--- resources/views/skills/index.blade.php
+++ resources/views/skills/index.blade.php
@@ -1,47 +1,49 @@
 @extends('layouts.master')
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-12">
-            @if (session('status'))
-                <div class="alert alert-success">{{ session('status') }}</div>
-            @endif
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                @if (session('status'))
+                    <div class="alert alert-success">{{ session('status') }}</div>
+                @endif
 
-            <div class="card mt-3">
-                <div class="card-header">
-                    <h4>Skills
-                        <a href="{{ route('skills.create') }}" class="btn btn-primary float-end">Add Skills</a>
-                    </h4>
-                </div>
-                <div class="card-body">
-                    <table class="table table-bordered table-striped">
-                        <thead>
-                            <tr>
-                                <th>Id</th>
-                                <th>Skills</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($skills as $skill)
+                <div class="card mt-3">
+                    <div class="card-header">
+                        <h4>Skills
+                            <a href="{{ route('skills.create') }}" class="btn btn-primary float-end">Add Skills</a>
+                        </h4>
+                    </div>
+                    <div class="card-body">
+                        <table class="table table-bordered table-striped">
+                            <thead>
                                 <tr>
-                                    <td>{{ $skill->id }}</td>
-                                    <td>{{ $skill->title }}</td>
-                                    <td>
-                                        <a href="{{ route('skills.edit', $skill->id) }}" class="btn btn-success">Edit</a>
-                                        <form action="{{ route('skills.destroy', $skill->id) }}" method="POST" style="display: inline-block;">
-                                            @csrf
-                                            @method('DELETE')
-                                            <button type="submit" class="btn btn-danger">Delete</button>
-                                        </form>
-                                    </td>
+                                    <th>Id</th>
+                                    <th>Skills</th>
+                                    <th>Actions</th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($skills as $skill)
+                                    <tr>
+                                        <td>{{ $loop->iteration }}</td>
+                                        <td>{{ $skill->title }}</td>
+                                        <td>
+                                            <a href="{{ route('skills.edit', $skill->id) }}"
+                                                class="btn btn-success">Edit</a>
+                                            <form action="{{ route('skills.destroy', $skill->id) }}" method="POST"
+                                                style="display: inline-block;">
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-danger">Delete</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
